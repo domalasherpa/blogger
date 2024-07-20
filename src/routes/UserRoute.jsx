4679634d77e510f0cf3614669df4490d8dd1e2cd
domalasherpa <domalasherpa9813@gmail.com>
2024-07-20 22:07:48 +0545

    user route updated

--- src/routes/UserRoute.jsx
+++ src/routes/UserRoute.jsx
@@ -1,15 +1,25 @@
 import User from "../components/User";
 import userLoader from "../components/loaders/userLoader";
+import UserLayout from "../layout/UserLayout";
 
 const UserRoute = [
     {
         path: "/:username",
-        element: <User />,
+        element: <UserLayout />,
         loader: userLoader,
+        id: "user",
         children: [
-            {    
-                path: "blogs",
-                element: <h1>Displays all the user blogs</h1>
+            {
+                path: "",
+                element: <User />
+            },
+            {
+                path: "about",
+                element: <About />
+            },
+            {
+                path: "following",
+                element: <Following />
             }
         ],
     }
