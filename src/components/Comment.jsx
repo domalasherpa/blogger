1e9d4617ff26eaec3d40cf1c7a25a76137143aad
domalasherpa <domalasherpa9813@gmail.com>
2024-07-20 22:10:01 +0545

    separate component for the comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
 import Avatar from "./users/Avatar";
 
-export default function Comment({props}){
-    const {user, date, content} = props;
+export default function Comment({user, date, content}){
+
     return (
-        <div className="comment">
-            <Avatar imageUrl={user.avatar} alt={`${user.username}'s image`}/>
-            <span>{user.username}</span>
-            <span>{date}</span>
+        <div className="comment text-xs mb-2 bg-slate-100 p-2 rounded-md">
+            <div className="flex h-10">
+                <Avatar imageUrl={user.avatar} alt={`${user.username}'s image`} className="h-7 w-7 object-contain"/>
+                <div className="ml-2">
+                    <p className="hover: underline"><Link to={`/@${user.username}`}>{user.username}</Link></p>
+                    <p className="italic">{date}</p>
+                </div>
+            </div>
             <p>{content}</p>
         </div>
 
