bed4b7187d9c0bc489a31c3eea60685c8bfd02a6
domalasherpa <domalasherpa9813@gmail.com>
2024-07-20 22:12:02 +0545

    classname added to avatar

--- src/components/users/Avatar.jsx
+++ src/components/users/Avatar.jsx
@@ -1,8 +1,8 @@
-const Avatar = ({ imageUrl, altText }) => {
+const Avatar = ({ imageUrl, altText , className=""}) => {
     return (<img
         src={imageUrl}
         alt={altText}
-        className="h-12 w-12 rounded-full object-cover shadow-md" />)
+        className={`h-12 w-12 rounded-full object-cover shadow-md ${className}`} />)
 }
 
 
