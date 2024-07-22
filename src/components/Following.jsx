7f0f4d02ae5da5044a4c88bc92ae35b0626b62fd
domalasherpa <domalasherpa9813@gmail.com>
2024-07-22 12:29:28 +0545

    minor changes

--- src/components/Following.jsx
+++ src/components/Following.jsx
@@ -1,50 +1,16 @@
-import { Link } from "react-router-dom";
-import { useState, useRef } from "react";
-import { IoIosMore } from "react-icons/io";
-import UserCard from "./users/UserCard";
+import { useRouteLoaderData } from "react-router-dom";
+import FollowCard from "./FollowCard";
 
-export default function Following({ follower }) {
-    const [hover, setHover] = useState(false);
-    const hoverTimeoutRef = useRef(null);
-
-    const handleMouseEnter = () => {
-        clearTimeout(hoverTimeoutRef.current); // Clear any existing timeout
-        setHover(true);
-    };
-
-    const handleMouseLeave = () => {
-        // Set a timeout to hide UserCard after 1 second (1000 milliseconds)
-        hoverTimeoutRef.current = setTimeout(() => {
-            setHover(false);
-        }, 200);
-    };
-
+export default function Following(){
+    const {following} = useRouteLoaderData("user");
     return (
-        <div className="flex text-xs justify-between flex-wrap relative">
-            <div className="flex">
-                <img
-                    src={follower.avatar}
-                    alt={`${follower.username}'s image`}
-                    className="h-6 w-6 rounded-full object-cover shadow-md"
-                />
-                <span className="self-end ml-1">
-                    <Link to={`/@${follower.username}`}>{follower.username}</Link>
-                </span>
-            </div>
-            <div
-                className="self-center rounded-full border-2 border-transparent hover:border-white cursor-pointer"
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-            >
-                <IoIosMore />
-            </div>
-            {hover && (
-                <UserCard
-                    user={follower}
-                    handleMouseEnter={handleMouseEnter}
-                    handleMouseLeave={handleMouseLeave}
-                />
-            )}
+        <div className="text-xs my-2">
+            {
+                following ? following.map(followed=>(
+                    <FollowCard avatar={followed.avatar} fullName={followed.fullName} bio={followed.bio} key={followed.id} username={followed.username}/>
+                ))
+                : <p>No Following</p>
+            }
         </div>
-    );
+    )
 }
