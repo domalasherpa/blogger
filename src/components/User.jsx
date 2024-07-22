c1bb8a7c658bb93c3dd1efc8a3b40cb5f96c3d61
domalasherpa <domalasherpa9813@gmail.com>
2024-07-22 11:55:43 +0545

    post null case added

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,11 +2,17 @@ import { useRouteLoaderData } from "react-router-dom";
 import Blog from "./Blog";
 
 export default function User() {
-  const user = useRouteLoaderData("user");
+    const user = useRouteLoaderData("user");
 
-  return (
-    <div className="my-5">
-        {user.posts && user.posts.map((blog) => <Blog post={blog} key={blog.id} />)}
-    </div>
-  )
+    return (
+        <div className="my-5 text-xs">
+            {
+                user.posts ? (
+                    user.posts.map((blog) => <Blog post={blog} key={blog.id} />)
+                ) : (
+                    <p>Have not posted anything.</p>
+                )
+            }
+        </div>
+    );
 }
